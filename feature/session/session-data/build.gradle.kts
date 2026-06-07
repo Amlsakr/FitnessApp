693c9b6948346eb280e772e7b70418d73c5fdc4b
Amlsakr <amlsakr9@gmail.com>
2026-06-07 17:04:19 +0300

    implement SETUP-004

--- feature/session/session-data/build.gradle.kts
+++ feature/session/session-data/build.gradle.kts
@@ -12,6 +12,7 @@ android {
 
     defaultConfig {
         minSdk = 30
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     compileOptions {
@@ -23,5 +24,12 @@ android {
 dependencies {
     implementation(project(":core:core-data"))
     implementation(project(":feature:session:session-domain"))
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.mlkit.pose.detection)
     testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.camera.view)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
